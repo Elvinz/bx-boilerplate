d88b0263b9a77f6af691117545f0a449d925e35c
Evgeny Mikulich <sidpunk@inbox.ru>
2015-08-31 19:03:15 +0500

    Add lang code to base template

--- components/your.namespace/your.component/templates/.default/template.php
+++ components/your.namespace/your.component/templates/.default/template.php
@@ -1,3 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+use \Bitrix\Main\Localization\Loc as Loc;
+Loc::loadMessages(__FILE__);
+
 // Composite mode
 //$this->setFrameMode(true);
