ecb1440001b69eb9d4240dea8a70b2cd591c8de2
John <sidpunk@inbox.ru>
2016-05-26 19:09:32 +0500

    Update ajax.php

--- components/shantilab.bxtools/base.component/ajax.php
+++ components/shantilab.bxtools/base.component/ajax.php
@@ -1,6 +1,9 @@
 <?
-define('NO_AGENT_CHECK', true); // disable agents
-define("STOP_STATISTICS", true); // disable statistic
+define("STOP_STATISTICS", true);
+define("NO_KEEP_STATISTIC", "Y");
+define("NO_AGENT_STATISTIC","Y");
+define("DisableEventsCheck", true);
+define("BX_SECURITY_SHOW_MESSAGE", true);
 
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php'); //include bitrix core
 
